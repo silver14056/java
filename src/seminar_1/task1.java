8c1e7aca1cb85ed4d779e7bc2861a6c22ef7e435
sergo book <sergo@book.ru>
2024-08-08 19:20:01 +0300

    seminar_1:task1 start

--- src/seminar_1/task1.java
+++ src/seminar_1/task1.java
@@ -1,4 +1,20 @@
 package seminar_1;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
 public class task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        LocalDateTime now = LocalDateTime.now();
+        DateFormat df =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        System.out.println(sdf.format(now));
+        System.out.println(df.format(now));
+        System.out.println("Сегодня и сейчас: " + now);
+
+
+
+    }
 }
