6cc428faf7bf74bed3141227ecc298e567f0f621
Serg Litov <Serg@litov.com>
2024-08-09 07:30:54 +0300

    seminar_1:task1 is Done

--- src/seminar_1/task1.java
+++ src/seminar_1/task1.java
@@ -1,18 +1,30 @@
 package seminar_1;
 
-import java.text.DateFormat;
+
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 
 public class task1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         LocalDateTime now = LocalDateTime.now();
-        DateFormat df =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        System.out.println(sdf.format(now));
-        System.out.println(df.format(now));
+        System.out.println(now);
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MMMM/dd HH*mm:ss");
+        String date = df.format(now);
+        System.out.println(date);
+        String time = now.format(df);
+        System.out.println(time);
+
+
         System.out.println("Сегодня и сейчас: " + now);
+
+        Date nowDate = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("E dd.MM.yyyy 'и время' HH:mm:ss");
+        System.out.println("Текущая дата: " + formatForDateNow.format(nowDate));
 
 
 
