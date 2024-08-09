e8eec18741a8b286d38bb21ddc70d188c52b46d0
sergo book <sergo@book.ru>
2024-08-09 23:26:12 +0300

    seminar2:task3

---
+++ src/seminar2t/task3.java
@@ -0,0 +1,52 @@
+package seminar2t;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class task3 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        Logger logger = Logger.getLogger("task2");
+        String jewels, stones;
+
+        logger.info("Введите драгоценную строку: ");
+        jewels = sc.nextLine();
+        if (jewels.isEmpty()) {
+            throw new Exception("Пустая строка");
+        }
+
+        logger.info("Введите строку \"кучу\": ");
+        stones = sc.nextLine();
+        if (stones.isEmpty()) {
+            throw new Exception("Пустая строка");
+        }
+
+        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < jewels.length(); i++) {
+//            if (stones.contains(String.valueOf(jewels.charAt(i)))) {
+//                if (!sb.toString().contains(String.valueOf(jewels.charAt(i)))) {
+//                    sb.append(jewels.charAt(i));
+//                    int count = stones.length() - stones.replace(String.valueOf(jewels.charAt(i)), "").length();
+//                    sb.append(count);
+//                }
+//            }
+//
+//        }
+        int count = 0;
+        for (char c1 : jewels.toCharArray()) {
+            for (char c2 : stones.toCharArray()) {
+                if (c1 == c2) {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                sb.append(c1);
+                sb.append(":");
+                sb.append(count);
+                sb.append(" ");
+            }
+            count = 0;
+        }
+        logger.info(sb.toString());
+    }
+}
