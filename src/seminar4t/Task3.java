581c95ec5b2628045e83fb4ab8561d35b5bebd74
Serg Litov <Serg@litov.com>
2024-08-25 10:10:14 +0300

    seminar4t is Done

--- src/seminar4t/Task3.java
+++ src/seminar4t/Task3.java
@@ -1,7 +1,32 @@
 package seminar4t;
 
+import java.util.Stack;
+
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String str = "{[()]}";
+        Task3 t = new Task3();
+        System.out.println(t.validate(str));
     }
+
+    public boolean validate(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()){
+            if (c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            } else if (stack.isEmpty()){
+                return false;
+            } else if (c == ')' && stack.pop()!= '('){
+                return false;
+            } else if (c == ']' && stack.pop() != '[') {
+                return false;
+            } else if (c == '}' && stack.pop() != '{') {
+                return false;
+            }
+
+        }
+        return stack.isEmpty();
+   }
+
+
 }
